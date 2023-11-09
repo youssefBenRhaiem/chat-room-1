8a0c33f81cd08f25d0e6fcb2993b9b632096cf45
youssefBenRhaiem <150254624+youssefBenRhaiem@users.noreply.github.com>
2023-11-09 19:19:07 +0100

    update 1

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,20 +1,12 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import { resolve } from "path";
+// import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default ({ mode }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
   return defineConfig({
     plugins: [react()],
-    build: {
-      rollupOptions: {
-        input: {
-          main: "src/main.jsx", // Your entry point file
-          main: resolve(__dirname, "index.html"),
-        },
-      },
-    },
     server: {
       proxy: {
         "/api": {
