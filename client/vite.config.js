fcad0b7e0d9faadff43e3498885eeeb0bf7977d7
youssefBenRhaiem <150254624+youssefBenRhaiem@users.noreply.github.com>
2023-11-09 20:52:08 +0100

    update client

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,23 +1,9 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 // import { resolve } from "path";
 
 // https://vitejs.dev/config/
-export default ({ mode }) => {
-  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
-  const proxy =
-    process.env.VITE_ENV == "development"
-      ? "http://localhost:3000"
-      : "https://little-chat-room-server.onrender.com";
-  return defineConfig({
-    plugins: [react()],
-    server: {
-      proxy: {
-        "/api": {
-          target: "https://little-chat-room-server.onrender.com",
-          changeOrigin: true,
-        },
-      },
-    },
-  });
-};
+export default defineConfig({
+  plugins: [react()],
+  server: {},
+});
