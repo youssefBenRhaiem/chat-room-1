ea3271f61ca3b6c8bdc8bd69693422a195031559
youssefBenRhaiem <150254624+youssefBenRhaiem@users.noreply.github.com>
2023-11-09 20:47:23 +0100

    update client

--- client/src/components/useSocketHook.jsx
+++ client/src/components/useSocketHook.jsx
@@ -2,16 +2,15 @@ import { useEffect, useState } from "react";
 import { io } from "socket.io-client";
 
 export default () => {
+  const path =
+    import.meta.env.VITE_ENV == "development"
+      ? "http://localhost:3000"
+      : "https://little-chat-room-server.onrender.com";
   const [socket, setSocket] = useState(
-    io(
-      import.meta.env.VITE_ENV == "development"
-        ? "http://localhost:3000"
-        : "https://little-chat-room-server.onrender.com",
-      {
-        autoConnect: false,
-        withCredentials: false,
-      }
-    )
+    io(path, {
+      autoConnect: false,
+      withCredentials: false,
+    })
   );
   useEffect(() => {
     socket.connect();
